39a1d212a7c8d1f47292b686bad6f667d364a7eb
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-09-14 15:37:43 +0200

    Add POJOs - close #2

---
+++ src/main/java/pl/mateuszkoczan/petclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package pl.mateuszkoczan.petclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/pl/mateuszkoczan/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package pl.mateuszkoczan.petclinic.model;
+
+public class Vet extends Person {
+}
