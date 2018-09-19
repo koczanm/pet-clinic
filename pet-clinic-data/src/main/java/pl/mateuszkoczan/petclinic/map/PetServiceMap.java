b5a7e0e2af1f8daa95a2e15c44ab3058b9ef9c67
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-09-19 22:36:37 +0200

    Implement map-based services - close #6

---
+++ pet-clinic-data/src/main/java/pl/mateuszkoczan/petclinic/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package pl.mateuszkoczan.petclinic.map;
+
+import pl.mateuszkoczan.petclinic.model.Pet;
+import pl.mateuszkoczan.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
