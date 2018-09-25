9d24baaeca6e1e56e4718b8e46731480c25cf165
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-09-25 17:44:23 +0200

    Add vets list and update controller - close #16

--- pet-clinic-web/src/main/java/pl/mateuszkoczan/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/pl/mateuszkoczan/petclinic/controller/VetController.java
@@ -1,13 +1,23 @@
 package pl.mateuszkoczan.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.mateuszkoczan.petclinic.service.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping("/vets")
-    public String showVets() {
+    public String showVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
