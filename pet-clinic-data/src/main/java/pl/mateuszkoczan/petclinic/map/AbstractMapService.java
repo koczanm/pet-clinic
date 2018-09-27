cc844bf698cb7405e2007ff0f6eba28dfe94f81a
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-09-27 17:55:13 +0200

    Add static resources - close #18

--- pet-clinic-data/src/main/java/pl/mateuszkoczan/petclinic/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/pl/mateuszkoczan/petclinic/map/AbstractMapService.java
@@ -17,7 +17,7 @@ abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
     }
 
     T save(T object) {
-        
+
         if (object != null) {
             if (object.getId() == null) {
                 object.setId(getNextId());
