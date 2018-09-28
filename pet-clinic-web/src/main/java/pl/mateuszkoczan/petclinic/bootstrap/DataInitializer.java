8f0929a42dbcb12769d94465509e01c2751933a2
Mateusz Koczan <xmateuszkoczan@gmail.com>
2018-09-29 01:20:42 +0200

    Add pet types to bootstrap- close #33

--- pet-clinic-web/src/main/java/pl/mateuszkoczan/petclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/pl/mateuszkoczan/petclinic/bootstrap/DataInitializer.java
@@ -3,18 +3,22 @@ package pl.mateuszkoczan.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.mateuszkoczan.petclinic.model.Owner;
+import pl.mateuszkoczan.petclinic.model.PetType;
 import pl.mateuszkoczan.petclinic.model.Vet;
 import pl.mateuszkoczan.petclinic.service.OwnerService;
+import pl.mateuszkoczan.petclinic.service.PetTypeService;
 import pl.mateuszkoczan.petclinic.service.VetService;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
     private final VetService vetService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, PetTypeService petTypeService, VetService vetService) {
         this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
         this.vetService = vetService;
     }
 
@@ -32,6 +36,16 @@ public class DataInitializer implements CommandLineRunner {
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners");
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
+
+        System.out.println("Loaded Pet Types");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
